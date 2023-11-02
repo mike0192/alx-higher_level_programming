0560bb9989e931db5ab18c2fb4145dd7221722b4
michael <michaelhagos163@gmail.com>
2023-11-02 12:36:18 -0700

    sonder

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def al(*argument):
-    argument[0] = ""
-    print("arguments: {}".format(argument))
+if __name__ == "__main__":
+    import sys
+    argLen = len(sys.argv)
+    if argLen == 1:
+        print("{} arguments:".format(argLen - 1))
+    elif argLen == 2:
+        print("{} argument:".format(argLen - 1))
+    else:
+        print("{} arguments:".format(argLen - 1))
+    for i in range(1, argLen):
+        print("{}: {}".format(i, sys.argv[i]))
+
