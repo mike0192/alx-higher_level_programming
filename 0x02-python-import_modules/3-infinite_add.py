a62f9b73c121684a8dbe4b46ea0376bfec65140e
michael <michaelhagos163@gmail.com>
2023-11-02 23:34:57 -0700

    sonder

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys, math
     result = 0
-    args = sys.argv[1:]:
-        result += sim(int(arg)for arg in args)
-        print("{}".format(result))
+    args = sys.argv[1:]
+    result = sum(int(arg)for arg in args)
+    print("{}".format(result))
