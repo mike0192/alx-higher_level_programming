29db3682d0f0e28c5ba963b70dbddbc6a30b8ee3
michael <michaelhagos163@gmail.com>
2023-11-03 05:50:24 -0700

    sonder

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,6 +14,11 @@ if __name__ == "__main__":
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-
-        result = int(a, b)
-        print("{} {} {}".format(a, operators, b))
+        if sys.argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
