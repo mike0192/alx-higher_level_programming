ecaf30395406940229df1327056b46bc74cdbf5a
michael <michaelhagos163@gmail.com>
2023-11-03 05:35:35 -0700

    sonder

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    import sys
+    operator = +, -, *, /
+    arg = sys.argv
+    if arg != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+   if operator != +, -, *, /:
+       print("Unknown operator. Available operators: +, -, * and /")
+       exit(1)
+       result = int(a, operator, b)
+       print("{} {} {}".format(a, operator, b)
+
