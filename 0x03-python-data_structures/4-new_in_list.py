1325e8c919f1436374b2539c4912799d97442641
michael <michaelhagos163@gmail.com>
2023-11-06 08:09:01 -0800

    sonder x breezy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copy = my_list.copy()
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
+        copy = my_list.copy()
         copy[idx] = element
         return copy
