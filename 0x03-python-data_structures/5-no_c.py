a0e5e8b3af0c1eec23b05a2f404b16a08a0eb7c1
michael <michaelhagos163@gmail.com>
2023-11-06 07:58:17 -0800

    sonder x breezy

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_strrrr = my_string.translate({ord(i): None for i in 'cC'))
+    new_strrrr = my_string.translate({ord(i): None for i in 'cC'})
     return new_strrrr
