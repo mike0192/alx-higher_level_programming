c27c3b0ac93f23b7beedfdfb5cff4607add554ab
michael <michaelhagos163@gmail.com>
2023-11-09 03:11:02 -0800

    sonder x breezy

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    n_dic = (key, value)
-    a_dictionary.update(n_dic)
+    a_dictionary[key] = value
     return a_dictionary
