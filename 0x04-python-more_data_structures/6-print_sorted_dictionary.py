c002d8c34d901f726b02c80848db63c0fd7c9d6a
michael <michaelhagos163@gmail.com>
2023-11-09 03:03:28 -0800

    sonder x breezy

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is None:
-        return
     for k in sorted(a_dictionary.keys()):
-        print("{}: {}:.format(k, a_dictionary.get(k)))
+        print("{}: {}".format(k, a_dictionary[k]))
