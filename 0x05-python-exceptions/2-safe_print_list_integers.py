ef1c4e23d8cfaae142f89edd401548740c303484
michael <michaelhagos163@gmail.com>
2023-11-20 09:49:36 -0800

    sonder

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,8 @@ def safe_print_list_integers(my_list=[], x=0):
     breezy = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list=[i]), end="")
-                breezy += 1
+            print("{:d}".format(my_list=[i]), end="")
+            breezy += 1
         except (ValueError, TypeError):
             pass
     print()
