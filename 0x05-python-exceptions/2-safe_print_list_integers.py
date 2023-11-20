8def8de464bac9620fec708a6a77523488e5d742
michael <michaelhagos163@gmail.com>
2023-11-20 10:22:00 -0800

    sonder

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     while i < x:
         try:
             print("{:d}".format(my_list=[i]), end="")
-            i += 1
+            breezy += 1
         except (ValueError, TypeError):
             pass
         i += 1
