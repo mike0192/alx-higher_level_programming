fdec18f7deab913edd17fc6d3da0fadc553d22b9
michael <michaelhagos163@gmail.com>
2023-11-21 10:13:46 -0800

    sonder x breezy

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 class Square:
+     """Defines a square."""
+
    def __init__(self, size):
+       """Constructor.
+        Args:
+            size: length of side of the square.
+        """
        self.__size = 3
