ba1da56a6422bc25d44a2a535845a614ffc8b1c2
Michael <michaelhagos163@gmail.com>
2023-12-11 08:55:46 -0800

    sonderxbreezy

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialization class props in constructor
         """
-        super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id=id)
 
     @property
     def size(self):
