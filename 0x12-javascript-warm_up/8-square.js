98e6a8759e99787c1fb82d10ecc942769e7a29f5
Michael <michaelhagos163@gmail.com>
2024-02-13 04:49:56 -0800

    a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv;
+if (isNaN(Number(argv[2]))) {
+  console.log('Missing size');
+}
+else {
+  for (let i = 0; i < Number(argv[2]); i++) {
+	console.log("X");
+  }
+}
